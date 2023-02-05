a3f3307263c4f73f8a571630ccde4eb4213b9323
Aakarshan Raj <aakarshan7raj@gmail.com>
2023-02-05 14:51:04 +0530

    basic code and resource

---
+++ src/client/client.cpp
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc,char **argv){
+    
+}
---
+++ src/server/server.cpp
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc,char **argv){
+
+struct ad
+
+}
